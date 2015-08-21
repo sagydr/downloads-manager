8d2ba1907f162ae5526522e694f8f50d52395e78
sagy drucker <sagysrael@hotmail.com>
2015-08-21 14:51:05 +0300

    added support of google analytics in options and readme pages

--- options.js
+++ options.js
@@ -1,6 +1,30 @@
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// google analytics:
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-66616134-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+// tracking buttons
+function trackButtonClick(e) {
+  _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+}
+
+/**
+ * Now set up your event handlers for the popup's `button` elements once the
+ * popup's DOM has loaded.
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  var buttons = document.querySelectorAll('button');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', trackButtonClick);
+  }
+});
+
 
 function Rule(data) {
     //console.log("data: " + JSON.stringify(data));
